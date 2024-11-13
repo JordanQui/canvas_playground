af835b91bbd5128a3609e9af76060d107d628479
jordan quiqueret <jordan.quiqueret@gmail.com>
2024-11-13 23:49:39 +0100

    update

---
+++ src/app/canvas/sketch0004/page.js
@@ -0,0 +1,15 @@
+import Sketch4 from "../../../components/Sketch0004";
+
+export default function GlissContainer() {
+     return (
+          <div
+               style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    "flex-direction": "column",
+               }}
+          >
+               <Sketch4 id="canvas1" />
+          </div>
+     );
+}
