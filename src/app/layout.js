1782ce30685656b88a86e40433fb1d7deb0bc6e1
jordan quiqueret <jordan.quiqueret@gmail.com>
2025-02-26 00:07:56 +0100

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,38 +1,40 @@
+"use client";
+
+import { useEffect } from "react";
 import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+     src: "./fonts/GeistVF.woff",
+     variable: "--font-geist-sans",
+     weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+     src: "./fonts/GeistMonoVF.woff",
+     variable: "--font-geist-mono",
+     weight: "100 900",
 });
 
-export const metadata = {
-     title: "Canvas",
-     description: "Jordan Olivier",
-     themeColor: "#000000",
-};
 
 export default function RootLayout({ children }) {
-  return (
-       <html lang="en">
-            <head>
-                 <meta name="apple-mobile-web-app-capable" content="yes" />
-                 <meta
-                      name="apple-mobile-web-app-status-bar-style"
-                      content="black-translucent"
-                 />
-            </head>
-            <body
-                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                 {children}
-            </body>
-       </html>
-  );
+     useEffect(() => {
+          document.body.classList.add("loaded");
+     }, []);
+
+     return (
+          <html lang="en">
+               <head>
+                    <meta name="apple-mobile-web-app-capable" content="yes" />
+                    <meta
+                         name="apple-mobile-web-app-status-bar-style"
+                         content="black-translucent"
+                    />
+               </head>
+               <body
+                    className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+               >
+                    {children}
+               </body>
+          </html>
+     );
 }
