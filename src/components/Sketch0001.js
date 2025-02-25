18b6fbde1bac1d212136609b62a769b782287d9e
jordan quiqueret <jordan.quiqueret@gmail.com>
2025-02-25 15:42:43 +0100

    update

--- src/components/Sketch0001.js
+++ src/components/Sketch0001.js
@@ -66,7 +66,7 @@ export default function Sketch0002({ id }) {
                     () =>
                          2 -
                          smoothedValues.valueLo +
-                         -smoothedValues.valueHi * 10,
+                         -smoothedValues.valueHi * 100,
                     0,
                     () => smoothedValues.valueMid1 * 500
                )
