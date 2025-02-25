0b5f298b7ab958ce40ff9a443a988f9bdfe7034f
jordan quiqueret <jordan.quiqueret@gmail.com>
2025-02-25 10:39:25 +0100

    update

--- src/app/page.js
+++ src/app/page.js
@@ -20,6 +20,7 @@ export default function Home() {
                     justifyContent: "center",
                     alignItems: "center",
                     height: "100vh",
+                    "background-color": "white"
                }}
           >
                {sketches.map((sketch, index) => (
